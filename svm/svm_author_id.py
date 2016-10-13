2e6fa0a6ff763b4ebdb4f0d7caa2826ab2e018a3
zarazi <zarazi@hotmail.com>
2016-10-13 09:32:53 +0700

    Fit & score

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,12 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+clf = svm.SVC(kernel='linear')
+clf.fit(features_train, labels_train)
 
+accuracy = clf.score(features_test, labels_test)
+print accuracy
 #########################################################
 
 
