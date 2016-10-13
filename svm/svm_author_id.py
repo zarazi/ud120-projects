ed1b7c377b04316b0aa0281ecd03e926e3b72c36
zarazi <zarazi@hotmail.com>
2016-10-13 09:44:18 +0700

    Measure training/predict time

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,10 +26,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 from sklearn import svm
 clf = svm.SVC(kernel='linear')
-clf.fit(features_train, labels_train)
 
-accuracy = clf.score(features_test, labels_test)
-print accuracy
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+clf.predict(features_test)
+print "predicting time:", round(time()-t1, 3), "s"
+
+# accuracy = clf.score(features_test, labels_test)
+# print accuracy
 #########################################################
 
 
